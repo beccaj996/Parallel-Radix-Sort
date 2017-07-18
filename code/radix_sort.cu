7a4ecad008a2cb304be1acf804eafbc201b1f033
JakeManfre <Jacob12@mail.usf.edu>
2017-07-17 23:06:59 -0400

    Added example in comments

--- code/radix_sort.cu
+++ code/radix_sort.cu
@@ -6,6 +6,80 @@
 unsigned int * valuesList;
 unsigned int totalNumbers;
 
+/***************** EXAMPLE ***********************
+
+ArrayVals: 			9, 31, 4, 18
+
+padded arrayVals:	09, 18, 04, 31
+
+Sort into histogram by leftMost digit (digit 1).
+This can be done in parallel. The order in which
+they are placed in their corresponding bucket
+doesn't matter. So we can use a thread for each
+element of the array and fill the buckets
+simultaneously.
+
+
+bucket: 	0  | 1  | 3
+		   -------------
+values:		09 | 18 | 31		
+			04 |	|  
+
+Once all the elements of the array have been placed
+in a bucket we must synch the threads before we 
+continue.
+
+__syncthreads();
+
+Iterate through each bucket. If a bucket contains
+more than 1 value in it then split it into more
+buckets based on the next digit to the right.
+bucket 0 contains 04 and 09 so sort these by digit 
+0 (the right most digit) to get:
+
+bucket: 	4  |  9
+		   ---------
+values:	   04  |  09
+
+Iterate through each bucket. If a bucket contains
+more than 1 value in it then split into more
+buckets based on the next digit to the right.
+Bucket 4 and 9 each contain only 1 value.
+
+Enter each bucket in the histogram onto a list
+starting from the smallest bucket and moving up
+to the largest bucket. So in this example add
+04 to the list and then 09 to the list and return
+this list back to the previous recursion call.
+This is line (34) above where it will continue onto
+the next iteration of its loop. The list that just
+returned to it will be sorted as we just saw.
+
+return list: 04,09
+
+histogram from line 17 should now look like this
+after the returned sorted list:
+
+bucket: 	0  | 1  | 3
+		   -------------
+values:		04 | 18 | 31		
+			09 |	| 
+
+Each bucket in the list now contains either a
+sorted list or only one element, which is also
+a sorted list. Therefore we can put these values
+into the original array in the sorted order by
+beginning with bucket 0 and moving up to bucket 3.
+
+arrayVals: 04, 09, 18, 31
+
+The array is sorted!
+
+**************************************************/
+
+
+
+
 __global__ unsigned int radixSort(unsigned int* values, int digit) {
 
 	// not sure about the implenetation of the histogram part
