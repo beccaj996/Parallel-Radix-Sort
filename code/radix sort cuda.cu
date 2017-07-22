81100a1f13be2f62ed0f79c0cc04378c02657354
JakeManfre <Jacob12@mail.usf.edu>
2017-07-21 21:07:43 -0400

    This works up to input size of 1024.
    Look at the kernel call in main. Right now only using 1 block and up to
    1024 threads.

--- code/radix sort cuda.cu
+++ code/radix sort cuda.cu
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
 
-int blockSize = 32;
-int numBlocks = 512;
+#define MAX 99;
+
+int blockSize = 256;
+int numBlocks;
+int* valueArray;
+int * dArray;
 
 //Declaration for partition sort
-__device__ void partition_by_bit(unsigned int *values, unsigned int bit);
+__device__ void partition_by_bit(int *values, int bit);
 
-__global__ void radix_sort(unsigned int *values)
+__global__ void radix_sort(int *values)
 {
-	printf("%d\n",values);
+	// printf("%d\n",values);
     int  bit;
     for( bit = 0; bit < 32; ++bit )
     {
@@ -21,9 +28,9 @@ __global__ void radix_sort(unsigned int *values)
 template<class T>
 __device__ T plus_scan(T *x)
 {
-    unsigned int i = threadIdx.x; // id of thread executing this instance
-    unsigned int n = blockDim.x;  // total number of threads in this block
-    unsigned int offset;          // distance between elements to be added
+    int i = threadIdx.x; // id of thread executing this instance
+    int n = blockDim.x;  // total number of threads in this block
+    int offset;          // distance between elements to be added
 
     for( offset = 1; offset < n; offset *= 2) {
         T t;
@@ -41,12 +48,12 @@ __device__ T plus_scan(T *x)
     return x[i];
 }
 
-__device__ void partition_by_bit(unsigned int *values, unsigned int bit)
+__device__ void partition_by_bit(int *values, int bit)
 {
-    unsigned int i = threadIdx.x;
-    unsigned int size = blockDim.x;
-    unsigned int x_i = values[i];          // value of integer at position i
-    unsigned int p_i = (x_i >> bit) & 1;   // value of bit at position bit
+    int i = threadIdx.x;
+    int size = blockDim.x;
+    int x_i = values[i];          // value of integer at position i
+    int p_i = (x_i >> bit) & 1;   // value of bit at position bit
 
     // Replace values array so that values[i] is the value of bit bit in
     // element i.
@@ -61,7 +68,7 @@ __device__ void partition_by_bit(unsigned int *values, unsigned int bit)
     // Compute number of True bits (1-bits) up to and including values[i], 
     // transforming values[] so that values[i] contains the sum of the 1-bits
     // from values[0] .. values[i]
-    unsigned int T_before = plus_scan(values);
+    int T_before = plus_scan(values);
 /*
     plus_scan(values) returns the total number of 1-bits for all j such that
     j <= i. This is assigned to T_before, the number of 1-bits before i 
@@ -74,10 +81,10 @@ __device__ void partition_by_bit(unsigned int *values, unsigned int bit)
     // the prefix sums computed, and that values[size-1] is the sum of all
     // elements in the array, which happens to be the number of 1-bits in 
     // the current bit position.
-    unsigned int T_total  = values[size-1];
+    int T_total  = values[size-1];
     // T_total, after the scan, is the total number of 1-bits in the entire array.
 
-    unsigned int F_total  = size - T_total;
+    int F_total  = size - T_total;
 /*    
     F_total is the total size of the array less the number of 1-bits and hence
     is the number of 0-bits.
@@ -93,32 +100,40 @@ __device__ void partition_by_bit(unsigned int *values, unsigned int bit)
 
 }
 
-int main(){
+int main(int argc, char **argv){
 	//FIXME: add arugment handler
-	int numElements = 4;
-	unsigned int valueArray[numElements];
-	printf("test\n");
+	int numElements = atoi(argv[1]);
+    numBlocks = numElements;
+	// valueArray[numElements];
 
-	//FOR TESTING ----
-	valueArray[0] = 15;
-	valueArray[1] = 1;
-	valueArray[2] = 8;
-	valueArray[3] = 4;
+	valueArray = (int *)malloc(sizeof(int)*numElements);
 
-	unsigned int * dArray = NULL;
-	cudaMalloc((void **)&dArray,sizeof(unsigned int) * numElements);
+    for (int i = 0; i < numElements; i++) {
+        valueArray[i] = (int) rand()%MAX;
+    }
 
-	cudaMemcpy(dArray,&valueArray,sizeof(unsigned int) * numElements, cudaMemcpyHostToDevice);
+    printf("PRINTING BEFORE:\n");
+    for(int i = 0; i < numElements;i++){
+        printf("%d, ",valueArray[i]);
+    }
 
-	radix_sort<<<numBlocks, blockSize>>>(dArray);
+	cudaMalloc((void **) &dArray, sizeof(int) * numElements);
 
-	cudaMemcpy(valueArray,&dArray,sizeof(unsigned int) * numElements, cudaMemcpyDeviceToHost);
+	cudaMemcpy(dArray,valueArray, sizeof(int) * numElements, cudaMemcpyHostToDevice);
+
+    // radix_sort<<<(numElements+blockSize-1)/blockSize, blockSize>>>(dArray);
+	radix_sort<<<1, numElements>>>(dArray);
+
+	cudaMemcpy(valueArray, dArray, sizeof(int) * numElements, cudaMemcpyDeviceToHost);
 
 	cudaFree(dArray);
 
+    printf("\n\nPRINTING AFTER:\n");
 	for(int i = 0; i < numElements;i++){
-		printf("%d \n",valueArray[i]);
+		printf("%d, ",valueArray[i]);
 	}
+
+    printf("DONE!\n");
 
 	return 0;
 }
